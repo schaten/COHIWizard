21c8022d6f14e8a3abf4945f5eed007061da89b7
hermy-sf <81504334+hermy-sf@users.noreply.github.com>
2024-05-11 10:27:02 +0200

    cleanup directories

---
+++ sources/yaml_editor/set_fonts.py
@@ -0,0 +1,31 @@
+# reading_file = open("./core/COHIWizard_GUI_v10_reduced.py", "r")
+
+# new_file_content = ""
+# for line in reading_file:
+#     stripped_line = line.strip()
+#     new_line = stripped_line.replace("font.setPointSize(10)", "font.setPointSize(12)")
+#     new_file_content += new_line +"\n"
+# reading_file.close()
+
+# writing_file = open("./core/COHIWizard_GUI_v10_reduced.py", "w")
+# writing_file.write(new_file_content)
+# writing_file.close()
+import re
+
+
+#defining the replace method
+def replace(file_path, text, subs, flags=0):
+    with open(file_path, "r+") as file:
+        #read the file contents
+        file_contents = file.read()
+        text_pattern = re.compile(re.escape(text), flags)
+        file_contents = text_pattern.sub(subs, file_contents)
+        file.seek(0)
+        file.truncate()
+        file.write(file_contents)
+
+    
+file_path = "./yaml_editor/yaml_editor_widget.py"
+text = "font.setPointSize(10)"
+subs = "font.setPointSize(11)"
+replace(file_path, text, subs)
